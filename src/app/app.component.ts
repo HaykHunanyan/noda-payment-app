5d99395bff5cd9253d78444f887cdcfea068c62e
HA_Hun <96hunanyan96@gmail.com>
2024-12-10 23:47:46 +0400

    feature added form section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { HeroComponent } from './components/hero/hero.component';
+import { FormComponent } from './components/form/form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent,HeroComponent],
+  imports: [HeaderComponent,HeroComponent,FormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
   templateUrl: './form.component.html',
-  styleUrl: './form.component.scss'
+  styleUrls: ['./form.component.scss'],
+  imports: [ReactiveFormsModule,CommonModule]
 })
 export class FormComponent {
+  form: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      businessType: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phoneNumber: ['', [Validators.required, Validators.pattern(/^[0-9]+$/)]],
+      hasNodaAccount: [false]
+    });
+  }
+
+  submitForm() {
+    if (this.form.invalid) {
+      this.markAllAsTouched();
+      return;
+    }
+    console.log(this.form.value);
+  }
+
+  markAllAsTouched() {
+    Object.values(this.form.controls).forEach((control) => control.markAsTouched());
+  }
 }
