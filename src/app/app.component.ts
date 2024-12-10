5aa085dbc197fcb86a33e4b852cdadc5f6d384bd
HA_Hun <96hunanyan96@gmail.com>
2024-12-11 01:31:56 +0400

    feature added carousel section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { FormComponent } from './components/form/form.component';
-
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { FooterComponent } from './components/footer/footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent,HeroComponent,FormComponent],
+  imports: [HeaderComponent,HeroComponent,FormComponent,CarouselComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { CommonModule } from '@angular/common';
+import { Navigation } from 'swiper/modules';
+import Swiper from 'swiper';
+
+Swiper.use([Navigation]);
 
 @Component({
   selector: 'app-carousel',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './carousel.component.html',
-  styleUrl: './carousel.component.scss'
+  styleUrls: ['./carousel.component.scss'],
 })
-export class CarouselComponent {
 
+export class CarouselComponent implements AfterViewInit {
+  images = [
+    '../../../assets/images/carousel-item1.jpg',
+    '../../../assets/images/carousel-item2.jpg',
+    '../../../assets/images/carousel-item1.jpg',
+    '../../../assets/images/carousel-item2.jpg',
+    '../../../assets/images/carousel-item1.jpg',
+    '../../../assets/images/carousel-item2.jpg',
+  ];
+
+  constructor(@Inject(PLATFORM_ID) private platformId: object) {}
+
+  ngAfterViewInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      // Initialize Swiper
+      new Swiper('.swiper', {
+        loop: true,
+        spaceBetween: 10,
+        slidesPerView: 3,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      });
+    }
+  }
 }
